d7c69a5c9cb102215ee444848c388e3568f06181
John Bruce <165712944+Jaybe-123456@users.noreply.github.com>
2026-03-31 16:24:47 -0700

    Add sitemap function to generate site URLs

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.vetios.tech/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.vetios.tech/about",
+      lastModified: new Date(),
+    },
+  ];
+}
